10eedd8f9d689d26c36cd186ffa0657a51390a51
ben-edde <ben.0xedde@gmail.com>
2021-12-29 19:55:29 +0800

    parsing json as ArrayList; show list of elements on GUI;

--- CanvasCollector/src/main/java/CanvasCollector.java
+++ CanvasCollector/src/main/java/CanvasCollector.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.snakeyaml.engine.v2.api.Load;
 import org.snakeyaml.engine.v2.api.LoadSettings;
 
@@ -5,7 +6,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class CanvasCollector
 {
@@ -18,31 +21,45 @@ public class CanvasCollector
             Load loader = new Load(settings);
             FileReader cfg_file = new FileReader(cfg_filepath);
             cfg_map = (LinkedHashMap<String, String>) loader.loadFromReader(cfg_file);
-        } catch (FileNotFoundException e)
+        }
+        catch (FileNotFoundException e)
         {
             e.printStackTrace();
         }
         return cfg_map;
     }
 
-    public static String ping()
+    public static ArrayList<Map<String, Object>> get_data(String resource)
     {
         LinkedHashMap<String, String> cfg_map = load_config("cfg/config.yaml");
         if (cfg_map != null)
+        {
             try
             {
-                URL url = new URL(cfg_map.get("API_ENDPOINT") + "/courses");
-//                System.out.println(url);
+                URL url = new URL(cfg_map.get("API_ENDPOINT") + "/" + resource);
                 HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.addRequestProperty("Authorization", "Bearer " + cfg_map.get("TOKEN"));
+
+                con.setRequestProperty("Authorization", "Bearer  " + cfg_map.get("TOKEN"));
+
                 con.setRequestMethod("GET");
                 con.setRequestProperty("Content-Type", "application/json");
-//                System.out.println(con.getResponseCode());
-                return con.getResponseMessage();
-            } catch (Exception e)
+
+                ObjectMapper mapper = new ObjectMapper();
+                ArrayList<Map<String, Object>> result_list;
+                result_list = mapper.readValue(con.getInputStream(), ArrayList.class);
+                return result_list;
+            }
+            catch (Exception e)
             {
                 e.printStackTrace();
             }
+        }
         return null;
+    }
+
+    public static ArrayList<Map<String, Object>> course_handler(String courseName, String courseID)
+    {
+        String resourcePath = "courses/" + courseID + "/" + "folders";
+        return get_data(resourcePath);
     }
 }
--- CanvasCollector/src/main/java/CollectorGUI.java
+++ CanvasCollector/src/main/java/CollectorGUI.java
@@ -1,21 +1,82 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class CollectorGUI extends Application {
+import java.util.ArrayList;
+import java.util.Map;
 
-    public static void main(String[] args) {
+public class CollectorGUI extends Application
+{
+    public static void main(String[] args)
+    {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage)
+    {
 //        CanvasCollector client=new CanvasCollector();
-        String result=CanvasCollector.ping();
-        Label label=new Label(result);
-        Scene scene=new Scene(label);
+        ObservableList<Map<String, Object>> elementList = FXCollections.observableArrayList();
+        ArrayList<Map<String, Object>> results = CanvasCollector.get_data("courses");
+        elementList.addAll(results);
+        StackPane pane = new StackPane();
+        Scene scene = new Scene(pane, 300, 150);
         primaryStage.setScene(scene);
+
+        ListView<Map<String, Object>> lv = new ListView<>(elementList);
+        lv.setCellFactory(param -> new ElementCell());
+        pane.getChildren().add(lv);
+
         primaryStage.show();
+    }
+
+    static class ElementCell extends ListCell<Map<String, Object>>
+    {
+        Map<String, Object> element;
+        HBox hbox = new HBox();
+        Label label = new Label("");
+        Pane pane = new Pane();
+        Button button = new Button("Select");
+
+        public ElementCell()
+        {
+            super();
+
+            hbox.getChildren().addAll(label, pane, button);
+            HBox.setHgrow(pane, Priority.ALWAYS);
+
+            button.setOnAction(event -> button_handler());
+        }
+
+        //TODO: download selected items here
+        void button_handler()
+        {
+            label.setText(Integer.toString((Integer) element.get("id")));
+        }
+
+        @Override
+        protected void updateItem(Map<String, Object> item, boolean empty)
+        {
+            super.updateItem(item, empty);
+            element = item;
+            setText(null);
+            setGraphic(null);
+
+            if (item != null && !empty)
+            {
+                label.setText((String) item.get("name"));
+                setGraphic(hbox);
+            }
+        }
     }
 }
